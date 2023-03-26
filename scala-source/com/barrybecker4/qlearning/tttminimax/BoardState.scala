50e2859cc28ea10786bf4457a140a83ec641ca2e
barrybecker4 <barrybecker4@gmail.com>
2023-03-26 05:46:58 -0700

    My human refactoring of the minimax version of ttt

---
+++ scala-source/com/barrybecker4/qlearning/tttminimax/BoardState.scala
@@ -0,0 +1,59 @@
+package com.barrybecker4.qlearning.tttminimax
+
+import com.barrybecker4.qlearning.tttminimax.BoardState.{Board, HUMAN, COMPUTER}
+import scala.annotation.tailrec
+
+
+object BoardState {
+  val HUMAN = 'X'
+  val COMPUTER = 'O'
+  type Board = List[Option[Char]]
+}
+
+case class BoardState(board: Board, player: Char) {
+
+  def this(player: Char) = this(List.fill(9)(None), player)
+
+  def makeHumanMove(position: Int): BoardState =
+    BoardState(board.updated(position, Some(player)), COMPUTER)
+
+  def makeComputerMove(position: Int): BoardState =
+    BoardState(board.updated(position, Some(player)), HUMAN)
+
+  def getAvailableMoves: List[Int] =
+    board.zipWithIndex.collect { case (None, index) => index }
+
+  def isGameOver: Boolean = hasPlayerWon(COMPUTER) || hasPlayerWon(HUMAN) || noMovesLeft
+
+  def getScore: Int = {
+    if (hasPlayerWon(HUMAN)) 1
+    else if (hasPlayerWon(COMPUTER)) -1
+    else 0
+  }
+
+  def print(): Unit = println(serialize())
+
+  def serialize(): String =
+    serializeRow(board.take(3)) + serializeRow(board.slice(3, 6)) + serializeRow(board.drop(6))
+
+  def printWinner(): Unit = {
+    val score = getScore
+    if (score < 0) println("The winner is the computer!")
+    else if (score > 0) println("The winner is you!")
+    else println("A tie!")
+  }
+
+  private def serializeRow(row: List[Option[Char]]): String =
+    row.map(c => if (c.isDefined) c.get else ".").mkString("[", "", "]") + "\n"
+
+  private def hasPlayerWon(player: Char): Boolean = {
+    val winningPositions = List(
+      List(0, 1, 2), List(3, 4, 5), List(6, 7, 8), // Rows
+      List(0, 3, 6), List(1, 4, 7), List(2, 5, 8), // Columns
+      List(0, 4, 8), List(2, 4, 6) // Diagonals
+    )
+    winningPositions.exists(position => position.forall(board(_).contains(player)))
+  }
+
+  private def noMovesLeft: Boolean = board.forall(_.isDefined)
+}
--- scala-source/com/barrybecker4/qlearning/tttminimax/TicTacToe.scala
+++ scala-source/com/barrybecker4/qlearning/tttminimax/TicTacToe.scala
@@ -1,102 +1,64 @@
 package com.barrybecker4.qlearning.tttminimax
+
+import com.barrybecker4.qlearning.tttminimax.BoardState.{COMPUTER, HUMAN}
 import scala.annotation.tailrec
 
+
 /**
-  * Unbeatable tic-tac-toe using minimax
-  * Mostly written by chatgpt, but debugged by me :)
+  * Unbeatable tic-tac-toe using minimax.
+  * Initially written by chatgpt, but debugged and refactored by me :)
   */
 object TicTacToe {
-  val HUMAN = 'X'
-  val COMPUTER = 'O'
-  type Board = List[Option[Char]]
-  val emptyBoard: Board = List.fill(9)(None)
 
   def main(args: Array[String]): Unit = {
-    var initialState: (Board, Char) = (emptyBoard, HUMAN)
+    var state: BoardState = new BoardState(HUMAN)
 
     println("Welcome to Tic-Tac-Toe!")
-    printBoard(initialState._1)
+    state.print()
 
-    while (!isGameOver(initialState._1)) {
+    while (!state.isGameOver) {
       val newState =
-        if (initialState._2 == HUMAN) humanMove(initialState)
-        else computerMove(initialState, COMPUTER)
-      printBoard(newState._1)
-      initialState = newState
+        if (state.player == HUMAN) humanMove(state)
+        else computerMove(state)
+      newState.print()
+      state = newState
     }
 
-    printBoard(initialState._1)
-    printWinner(initialState._1)
+    state.print()
+    state.printWinner()
   }
 
   @tailrec
-  private def humanMove(state: (Board, Char)): (Board, Char) = {
+  private def humanMove(state: BoardState): BoardState = {
     println("Your move! Enter a number from 1 to 9 to make your move:")
-    val index = scala.io.StdIn.readInt() - 1
-    if (index < 0 || index > 8 || state._1(index).isDefined) {
+    val position = scala.io.StdIn.readInt() - 1
+    if (position < 0 || position > 8 || state._1(position).isDefined) {
       println("Invalid move. Please choose an empty space.")
       humanMove(state)
     } else {
-      (state._1.updated(index, Some(state._2)), COMPUTER)
+      state.makeHumanMove(position)
     }
   }
 
-  def computerMove(state: (Board, Char), computerPlayer: Char): (Board, Char) = {
-    val (bestScore, bestMove) = minimax(state._1, computerPlayer)
-    assert(bestMove >= 0, s"bestMove=$bestMove  must be >=0 or it means that there was no move to make. \nBoard=" +
-      serializeBoard(state._1))
-    (state._1.updated(bestMove, Some(computerPlayer)), 'X')
-  }
+  def computerMove(state: BoardState): BoardState = 
+    val (bestScore, bestMove) = minimax(state)
+    state.makeComputerMove(bestMove)
 
-  private def minimax(board: Board, player: Char): (Int, Int) = {
-    val availableMoves = getAvailableMoves(board)
-    if (isGameOver(board)) {
-      (getScore(board), -1)
+  private def minimax(state: BoardState): (Int, Int) = {
+    val availableMoves = state.getAvailableMoves
+    if (state.isGameOver) {
+      (state.getScore, 0)  // computer won
     } else {
-      val initial = if (player == COMPUTER) Int.MaxValue else Int.MinValue
-      val (bestScore, bestMove) = availableMoves.foldLeft((initial, -1)) {
+      val initial = if (state.player == COMPUTER) Int.MaxValue else Int.MinValue
+      val (bestScore: Int, bestMove: Int) = availableMoves.foldLeft((initial, -1)) {
         case ((score, move), m) =>
-          val newBoard = board.updated(m, Some(player))
-          val (newScore, _) = minimax(newBoard, if (player == HUMAN) COMPUTER else HUMAN)
-          if (player == 'X' && newScore > score || player == COMPUTER && newScore < score) (newScore, m) else (score, move)
+          val newBoard = state.board.updated(m, Some(state.player))
+          val (newScore, _) = minimax(BoardState(newBoard, if (state.player == HUMAN) COMPUTER else HUMAN))
+          if (state.player == 'X' && newScore > score || state.player == COMPUTER && newScore < score) (newScore, m)
+          else (score, move)
       }
       (bestScore, bestMove)
     }
   }
 
-  private def getAvailableMoves(board: Board): List[Int] =
-    board.zipWithIndex.collect { case (None, index) => index }
-
-  private def isGameOver(board: Board): Boolean =
-    hasPlayerWon(board, HUMAN) || hasPlayerWon(board, COMPUTER) || board.forall(_.isDefined)
-
-  private def hasPlayerWon(board: Board, player: Char): Boolean = {
-    val winningPositions = List(
-      List(0, 1, 2), List(3, 4, 5), List(6, 7, 8), // Rows
-      List(0, 3, 6), List(1, 4, 7), List(2, 5, 8), // Columns
-      List(0, 4, 8), List(2, 4, 6) // Diagonals
-    )
-    winningPositions.exists(position => position.forall(board(_).contains(player)))
-  }
-
-  private def printWinner(board: Board): Unit = {
-    val score = getScore(board)
-    if (score < 0) println("The winner is the computer!")
-    else if (score > 0) println("The winner is you!")
-    else println("A tie!")
-  }
-
-  private def getScore(board: Board): Int = {
-    if (hasPlayerWon(board, HUMAN)) 1
-    else if (hasPlayerWon(board, COMPUTER)) -1
-    else 0
-  }
-
-  def printBoard(board: Board): Unit = println(serializeBoard(board))
-
-  def serializeBoard(board: Board): String =
-    serializeRow(board.take(3)) + serializeRow(board.slice(3, 6)) + serializeRow(board.drop(6))
-
-  private def serializeRow(row: List[Option[Char]]): String =
-    row.map(c => if (c.isDefined) c.get else ".").mkString("[", "", "]") + "\n"
 }
